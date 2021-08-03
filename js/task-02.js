b7a960bea4a4857bc59608be7104cfd699ecf8c8
EgorSokurenko <72275136+EgorSokurenko@users.noreply.github.com>
2021-08-03 13:55:12 +0300

    2 таск

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,28 @@ const ingredients = [
   'Зелень',
   'Приправы',
 ];
+const ingredientsList = document.querySelector('#ingredients');
+
+// forEACH
+
+// const ingredientsEl = [];
+// ingredients.forEach(ingredient => {
+//   const element = document.createElement('li');
+//   const link = document.createElement('a');
+//   link.href = 'https://www.youtube.com/watch?v=Q-qgMT1QrwQ';
+//   link.textContent = ingredient;
+//   element.appendChild(link);
+//   ingredientsEl.push(element);
+// });
+
+// MAP
+
+const ingredientsEl = ingredients.map(ingredient => {
+  const element = document.createElement('li');
+  const link = document.createElement('a');
+  link.href = 'https://www.youtube.com/watch?v=Q-qgMT1QrwQ';
+  link.textContent = ingredient;
+  element.appendChild(link);
+  return element;
+});
+ingredientsList.append(...ingredientsEl);
