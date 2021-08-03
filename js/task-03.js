76fbaee67447d55074d6d7a954041d13d8782b05
EgorSokurenko <72275136+EgorSokurenko@users.noreply.github.com>
2021-08-03 23:13:05 +0300

    таск 3!!!!

--- js/task-03.js
+++ js/task-03.js
@@ -12,3 +12,28 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+const gallery = document.querySelector('#gallery');
+
+const imagesEl = images.map(image => {
+  const string = `<li><img class='image'src=${image.url} alt=${image.alt}></li>`;
+  console.log(string);
+
+  return string;
+});
+console.log(imagesEl);
+gallery.insertAdjacentHTML('beforeend', imagesEl);
+
+// const imagesEl = images.map(image => {
+//   const element = document.createElement('li');
+
+//   const imageEl = document.createElement('img');
+//   imageEl.src = image.url;
+//   imageEl.alt = image.alt;
+//   imageEl.width = 300;
+//   imageEl.height = 300;
+//   element.append(imageEl);
+//   console.log(element);
+//   return element;
+// });
+// gallery.append(...imagesEl);
